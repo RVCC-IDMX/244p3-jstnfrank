818be8d72f2a9a77c387ae94f06275a2a09f0f8f
Justin Frank <justinfrank1217@gmail.com>
2020-10-17 18:09:21 -0400

    Copied Code

--- js/app.js
+++ js/app.js
@@ -1,234 +1,200 @@
 /*
-function dishTotalAmt(dish)
+checkPlease()
 
+take order using a series of prompt modal popups
+write the bill out to the console
+write the bill out to the page
+*/
+
+/*
+function dishTotalAmt(dish)
 take an object dish and return
 the product of cost * quant
 */
-function dishTotalAmt(dish){
-  return dish.cost * dish.quantity;
-}
+
+const dishTotalAmt = (dish) => dish.cost * dish.quantity,
 
 /*
-function subTotalAmt(dishTotals) {
-  let subTotal = 0; 
-  for (let i = 0; i< dishTotals.length; i++){
-    subTotal += dishTotals[i];
-  }
-  return subTotal; 
+*/
+const subTotalAmt = (dishTotals) =>
+{
+  dishTotals.reduce((sum,current) => sum + current, 0); 
 }
+
+
 /*
 calculate tax at 7%
 */
 
-function taxAmt(num) {
-  return num * 0.07;
-}
+const taxAmt = (num) => num * 0.07;
+
 /*
-function totalAmt(subTotal) = number
-add tax to subTotal and return total
 */
-function totalAmt(subTotal){
-  return subTotal + taxAmt(subTotal); 
-}
-
-
-
+const totalAmt = (subTotal) => subTotal + taxAmt(subTotal);
 
 /*
-functions that return string 
+FUNCTIONS THAT RETURN STRING 
 */
 
 /*
 function makeBillTitle()
 */
-function makeBillTitle(){
-  return 'Your Bill'; 
-}
+const makeBillTitle = () => 'Your Bill';
 
 /*
-function makeBillSeparator()
+function makeBillSeperator()
 */
 
-function makeBillSeparator(){
-  return '---------';
-}
+const makeBillSeperator = () => '--------';
 
 /*
 function makeDollar(cost)
+use toFixed() for 2 decimal numbers
+it returns a string
 */
-function makeDollar(cost){
-  // use toFixed()for 2 decimal numbers
-  // it returns a string
-  let str = cost.toFixed(2);
-  return '$' + str;
-}
+
+const makeDollar = (cost) => '$' + cost.toFixed(2);
 
 /*
 function makeLineItem(dish)
 */
 
-function makeLineItem(dish){
+const makeLineItem = (dish) => {
+  /*
+  dish is an object
+  example return: Hamburger: $3.49 * 5 = $17.45
+  */
+ let str;
+
+ //
+ const { food, cost, quantity} = dish;
+
+ // change costs to dollar amounts
+ let dishCost = makeDollar(cost);
+ let lineCost = makeDollar(dishTotalAmt(dish));
+
+ // put string together
+ str = `${food} ${dishCost} * ${quantity} = ${lineCost}`;
+
+ // add a return character to end of line
+ str += '\n';
+ return str;
+};
+
+const makeLineItems = (dishes) =>
+dishes.reduce((sum,current)=> sum + makeLineItem(current), '');
+
+const makeSubtotalLine = (subTotal) =>
+'Subtotal         ' + makeDollar(subTotal);
+
+const makeTaxLine = (tax) => '+ 7% tax          ' + makeDollar(tax);
+
+const makeTotalLine = (total) => 'Total         ' + makeDollar(total);
+
 /*
-dish is an object
-example return: Hamburger: $3.49 * 5 = $17.45
+data array
 */
 
-let str;
+const dishMenu = [];
 
-// pull out object properties
-const food = dish.food;
-const cost = dish.cost;
-const quantity = dish.quantity;
-
-// change costs to dollar amounts
-let dishCost = makeDollar(dish.cost);
-let lineCost = makeDollar(dishTotalAmt(dish));
-
-// put struing together
-str= `${food} ${dishCost} * ${quantity} = ${lineCost}`;
-// add a return character to end of line
-str += '\n';
-return str;
-}
-function makeLineItems(dishes){
-  let str = '';
-  for (let i = 0; i< dishes.length; i++){
-    str += makeLineItem(dishes[i]);
-  }
-  return str;
-}
-function makeSubtotalLine(subTotal){
-  return 'Subtotal         ' + makeDollar(subTotal);
-}
-  function makeTaxLine(tax){
-    return '+ 7% tax        ' + makeDollar(tax);
-  } 
-   
-   
-  function makeTotalLine(total){
-    return 'Total            ' + makeDollar(total);
-  }
-
-
-
-
-/* 
-data array 
-*/
-
-const orderedMeal = [];
-
-orderedMeal.push({
-  food: 'Hamburger',
-  cost: 3.49,
-  quantity:5, 
+dishMenu.push({
+food: 'Hamburger: ',
+cost: 3.49,
 });
 
-orderedMeal.push({
-  food: 'Fries',
+dishMenu.push({
+  food: 'Fries: ',
   cost: 1.29,
-  quantity:3, 
-});
+  });
 
-orderedMeal.push({
-  food: 'Soda',
-  cost: 1.79,
-  quantity:4, 
-});
+  dishMenu.push({
+    food: 'Soda: ',
+    cost: 1.79,
+    });
 
-orderedMeal.push({
-  food: 'Sundae',
-  cost: 1.99,
-  quantity:1, 
-});
+    dishMenu.push({
+      food: 'Sundae: ',
+      cost: 1.99,
+      });
 
-/*
-create output to console
-*/
+      /*
+      function takeAnOrder(dishMenu)
+      */
+     function promptQuantity(dish){
+      let quantity = +prompt(`How many ${dish.food} would you like?`, '');
+      if (!isFinite(quantity)){
+        console.log(quantity);
+        quantity = 0;
+      }
+      return quantity;
+     }
 
-function makeDishTotalsArray(dishes) {
-  let arr = [];
-  for (let i = 0; i < dishes.length; i++){
-    arr.push(dishTotalAmt(dishes[i]));
-  }
-  return arr;
-}
-let dishTotalsArray = makeDishTotalsArray(orderedMeal);
+     function takeAnOrder(dishMenu){
+       let order = dishMenu.map((dish)=>{
+         return (dish.quantity = promptQuantity(dish));
+       });
+       return dishMenu;
+     }
 
-let subTotal = subTotalAmt(dishTotals);
+     const orderedMeal = takeAnOrder(disjMenu);
 
-let tax = taxAmt(subTotal);
+     /*
+     function writeOut that returns
+     function writeToPage()
+     */
 
-let total = totalAmt(subTotal);
+     function writeOut(){
+       /*
+       document.querySelector is expensive call
+       use closure to do only once
+       */
+      const bill = document.querySelector(' .bill');
+      function writeToPage(str) {
+        let pre = document.createElement('pre');
+        pre.innerText = str;
+        bill.appendChild(pre);
+      }
+      return writeToPage;
+     }
 
-console.log(makeBillTitle());
-console.log(makeBillSeparator());
-console.log(makeLineItems(orderedMeal));
-console.log(makeBillSeparator());
-console.log(makeSubtotalLine(subTotal));
-console.log(makeTaxLine(tax));
-console.log(makeBillSeparator());
-console.log(makeTotalLine(total));
+     const writeToPage = writeOut();
 
+     function makeDishTotalsArray(dishes) {
+       return dishes.map((dish) => dishTotalAmt(dish));
+     }
 
+     /*
+     create data for output
+     */
 
+     let makeDishTotalsArray = makeDishTotalsArray(orderedMeal);
 
+     let subTotal = subTotalAmt(makeDishTotalsArray);
 
-/*
+     let tax = taxAmt(subTotal);
 
-Rewrite this code as needed to:
+     let total = totalAmt(subTotal);
 
-Print out to the console a check
-for a family that orders:
+     /*
+     write bill out to console
+     */
+    console.log(makeBillTitle());
+    console.log(makeBillSeperator());
+    console.log(makeLineItems(orderedMeal));
+    console.log(makeBillSeperator());
+    console.log(makeSubtotalLine(subTotal));
+    console.log(makeTaxLine(tax));
+    console.log(makeBillSeperator());
+    console.log(makeTotalLine(total));
 
-5 hamburgers
-3 fries
-4 sodas
-1 sundae
-
-The check must show a list of what was ordered, with subtotals, 
-added tax, and the grand total.
-
-Your Bill
----------
-Hamburger: $3.49 x 5 = $17.45
-Fries:     $1.29 x 3 =  $3.87
-Soda:      $1.79 x 4 =  $7.16
-Sundae:    $1.99 x 1 =  $1.99
----------
-Subtotal               $30.47
-+ 7% tax               $ 2.13
----------
-Total                  $32.60
-
-Hint: Redesign the object to hold the
-quantity ordered.
-
-Here's an example:
-
-{
-  food: "Hamburger",
-  cost: 3.49,
-  quantity: 5,
-}
-functions that return numbers 
-
-
-function dishTotalAmt(dish) = object
-function subTotalAmt(dishTotals) = array
-function taxAmt(num) = number
-function totalAmt(subTotal) = number
-
-functions that return strings 
-
-
-function makeBillTitle()
-function makeBillSeparator()
-function makeDollar(cost)
-function makeLineItem(dish)
-function makeLineItems(dishes)
-function makeSubtotalLine(subTotal)
-function makeTaxLine(tax)
-function makeTotalLine(total)
-
-*/
+    /*
+    write bill out to page
+    */
+   writeToPage(makeBillTitle());
+   writeToPage(makeBillSeperator());
+   writeToPage(makeLineItems(orderedMeal));
+   writeToPage(makeBillSeperator());
+   writeToPage(makeSubtotalLine(subTotal));
+   writeToPage(makeTaxLine(tax));
+   writeToPage(makeBillSeperator());
+   writeToPage(makeTotalLine(total));
